6be3f47b41e4608078cbbee08944ae579bab4199
astrika <astrika.0914@gmail.com>
2019-04-11 14:12:17 -0500

    Edits to PrepareEWandNW.R

--- PrepareEWandNW.R
+++ PrepareEWandNW.R
@@ -2,9 +2,9 @@
 #Calculating edge weights for EW_dmGWAS input, matching PPI edges to node weights.
 #04/10/2019
 
-GE.control <- read.table("PATH_TO_GENE_EXPRESSION_DATA_FOR_CONTROL")
-GE.case <- read.table("PATH_TO_GENE_EXPRESSION_DATA_FOR_CASE")
-PPI = read.table("PATH_TO_PPI")
+GE.control <- read.table("PATH_TO_GENE_EXPRESSION_DATA_FOR_CONTROL", as.is = T)
+GE.case <- read.table("PATH_TO_GENE_EXPRESSION_DATA_FOR_CASE", as.is = T)
+PPI <- read.table("PATH_TO_PPI")
 
 
 #Obtaining PCC
@@ -19,7 +19,26 @@ casePCC <- obtainPCC(GE.case)
 #matching to PPI
 matchPCCtoPPI <- function(PCCvalues, PPI){
   
-  #match to PPI
+  genes.with.expression <- row.names(GE.case)
+  
+  PPI_PCC <- data.frame()
+  
+  for(i in seq(1:nrow(PPI))){
+    
+    is.element(PPI[i, 1], genes.with.expression) -> check1
+    is.element(PPI[i, 2], genes.with.expression) -> check2
+    
+    if( !check1 | (!check2) )next
+    
+    PPI_PCC[i,1] <- PPI[i, 1]
+    PPI_PCC[i,2] <- PPI[i, 2]
+    PPI_PCC[i,3] <- PCCvalues[PPI[i, 1],PPI[i, 2]]
+  }
+  
+  PPI_PCC_rmNA <- na.omit(PPI_PCC)
+  
+  PPI_PCC_rmNA_rmSI <- PPI_PCC_rmNA[which(PPI_PCC_rmNA[,1]!=PPI_PCC_rmNA[,2]), ]
+  
   return(PPI_PCC_rmNA_rmSI)
 }
 
