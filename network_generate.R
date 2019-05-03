27cf7a24e084e4db0571da9d8aa659d2c8b17584
Fangfang Yan <30477318+fangfang0906@users.noreply.github.com>
2019-05-03 17:32:51 -0500

    Update network_generate.R

--- network_generate.R
+++ network_generate.R
@@ -1,76 +1,38 @@
-load("C:\\Users\\fyan\\Desktop\\18cleft\\edge_weight.RData")
-ls()
-dim(df)
-head(df)
-df <- na.omit(df)
-head(df)
-dim(df)
-summary(df$e)
-scale(df$e) -> df$e_scale
-head(df)
-write.table(df,"edge_weight.txt",sep="\t",row.names=FALSE)
-write.table(df[,c(1,2,9)],"edge_weight_scale.txt",sep="\t",row.names=FALSE)
-ls(0
-)
-ls()
-gwas_asian <- read.delim("C:/Users/fyan/Desktop/18cleft/dmGWAS_R/PASCAL_original_10_26/adjusted/GWAS_Asian_adjusted.txt",as.is=T)
-head(gwas_asian)
-gwas_asian$e <- qnorm(1- gwas_asian$pvalue/2)
-head(gwas_asian)
-hist(gwas_asian$e)
-gwas_asian$e_scale <- scale(gwas_asian$e)
-head(gwas_asian)
-write.table(gwas_asian,"node_weight.txt",sep="\t",row.names=FALSE)
-write.table(gwas_asian[,c(1,4)],"node_weight_scale.txt",sep="\t",row.names=FALSE)
-q()
-getwd()
+## Load edge weight and node weight file
 library(igraph)
-edge.weight = read.table("C:/Users/fyan/EW_dmGWAS/1_29/edge_weight_scale.txt", header=T, as.is=T)
-head(edge.weight)
 edge.weight = read.table("C:/Users/fyan/EW_dmGWAS/1_29/edge_weight_scale.txt", header=F, as.is=T)
-dim(edge.weight)
-G = graph.data.frame(edge.weight[,1:2], weight=edge.weight[,3])
+node.weight = read.table("C:/Users/fyan/EW_dmGWAS/1_29/Asian_node_weight_scale.txt", as.is=T)
+
+## Generate graph
 G = graph.data.frame(edge.weight[,1:2], directed=F)
 E(G)$weight = edge.weight[, 3]
-res.mat = read.delim("C:/Users/fyan/EW_dmGWAS/1_29/Asian.lambda_1_permutated.txt", as.is=T)
-head(res.mat)
-res.mat = res.mat[order(res.mat[, "z_perm"]), ]
-head(res.mat)
-res.mat = res.mat[order(res.mat[, "z_perm"], decreasing=T), ]
-head(res.mat)
-genes = strsplit(res.mat[1,1], split=" ")[[1]]
-genes
-induced.subgraph(G, genes) -> subG
-vcount(subG)
-cbind(get.edgelist(subG), E(subG)$weight)
-cbind(V(subG)$name, V(subG)$weight)
-node.weight = read.table("C:/Users/fyan/EW_dmGWAS/1_29/Asian_node_weight_scale.txt", as.is=T)
-head(node.weight)
 z_node = node.weight[,2]
-names(z_node) = node.weight[m1]
 names(z_node) = node.weight[,1]
 V(G)$weight = z_node[V(G)$name]
- induced.subgraph(G, genes) -> subG
-vcount(G)
-ecount(G)
-summary(E(G)$weight)
-summary(V(G)$weight)
-dim(node.weight)
-length(intersect(node.weight[,1], V(G)$name))
-genes
-ecount(subG)
-get.edgelist(subG)
-cbind(get.edgelist(subG), E(subG)$weight)
-cbind(V(subG)$name, V(subG)$weight)
-sig.res.mat = res.mat[res.mat$z_perm > 1.96, ]
-sapply(sig.res.mat[,1], function(u)strsplit(u, split=" ")[[1]] ) -> module.genes
-length(module.genes)
-dim(res.mat)
-dim(sig.res.mat)
-module.genes = unique(unlist(module.genes))
-length(module.genes)
-sort(module.genes)
-history()
-history()
-history(1000)
 
+## Load output file generated from EW_dmGWAS
+res.mat = read.delim("C:/Users/fyan/EW_dmGWAS/1_29/Asian.lambda_1_permutated.txt", as.is=T)
+res.mat = res.mat[order(res.mat[, "z_perm"], decreasing=T), ]
+head(res.mat)
+
+## Top 1 module
+top1_genes = strsplit(res.mat[1,1], split=" ")[[1]]
+top1_genes
+top1_subG <- induced.subgraph(G, top1_genes)
+vcount(top1_subG)
+top1_edge = cbind(get.edgelist(top1_subG), E(top1_subG)$weight)
+top1_node = cbind(V(top1_subG)$name, V(top1_subG)$weight)
+write.table(top1_edge,file="top1_edge.txt", row.names=FALSE, sep="\t")
+write.table(top1_node,file="top1_node.txt", row.names=FALSE, sep="\t")
+
+## All significant modules
+sig.res.mat <- res.mat[res.mat$z_perm > 1.96, ]
+module.genes <- sapply(sig.res.mat[,1], function(u)strsplit(u, split=" ")[[1]] )
+module.genes <- unique(unlist(module.genes))
+length(module.genes)
+All_subG <- induced.subgraph(G, All_genes)
+vcount(All_subG)
+All_edge = cbind(get.edgelist(All_subG), E(All_subG)$weight)
+All_node = cbind(V(All_subG)$name, V(All_subG)$weight)
+write.table(All_edge,file="All_edge.txt", row.names=FALSE, sep="\t")
+write.table(All_node,file="All_node.txt", row.names=FALSE, sep="\t")
