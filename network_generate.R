d3b43d60a9d9370e6a14254944f187ae6f1a7d06
Fangfang Yan <30477318+fangfang0906@users.noreply.github.com>
2019-05-06 17:39:44 -0500

    Update network_generate.R

--- network_generate.R
+++ network_generate.R
@@ -1,38 +1,35 @@
-## Load edge weight and node weight file
-library(igraph)
-edge.weight = read.table("C:/Users/fyan/EW_dmGWAS/1_29/edge_weight_scale.txt", header=F, as.is=T)
-node.weight = read.table("C:/Users/fyan/EW_dmGWAS/1_29/Asian_node_weight_scale.txt", as.is=T)
+## Change directory to your directory
+setwd("path to your directory")
+
+## Load edge weight file, node weight file, EW_dmGWAS output file
+edge.weight = read.table("path to your edge weight file", header=F, as.is=T)
+node.weight = read.table("path to your node weight file", as.is=T)
+res.mat = read.delim("path to your EW_dmGWAS output file", as.is=T)
+
+res.mat = res.mat[order(res.mat[, "z_perm"], decreasing=T), ]
+## All significant modules
+sig.res.mat <- res.mat[res.mat$z_perm > 1.96, ]
 
 ## Generate graph
+library(igraph)
 G = graph.data.frame(edge.weight[,1:2], directed=F)
 E(G)$weight = edge.weight[, 3]
 z_node = node.weight[,2]
 names(z_node) = node.weight[,1]
 V(G)$weight = z_node[V(G)$name]
 
-## Load output file generated from EW_dmGWAS
-res.mat = read.delim("C:/Users/fyan/EW_dmGWAS/1_29/Asian.lambda_1_permutated.txt", as.is=T)
-res.mat = res.mat[order(res.mat[, "z_perm"], decreasing=T), ]
-head(res.mat)
+## Generate top n modules 
+top_modules <- function(x){
+top_genes <- sapply(sig.res.mat[1:x,1], function(u)strsplit(u, split=" ")[[1]])
+module_genes <- unique(unlist(top_genes))
+subG <- induced.subgraph(G, module_genes)
+edge <- cbind(get.edgelist(subG), E(subG)$weight)
+node <- cbind(V(subG)$name, V(subG)$weight)
+write.table(edge,file="edge_top.txt", row.names=FALSE, quote=F, sep="\t")
+write.table(node,file="node_top.txt", row.names=FALSE, quote=F, sep="\t")}
 
-## Top 1 module
-top1_genes = strsplit(res.mat[1,1], split=" ")[[1]]
-top1_genes
-top1_subG <- induced.subgraph(G, top1_genes)
-vcount(top1_subG)
-top1_edge = cbind(get.edgelist(top1_subG), E(top1_subG)$weight)
-top1_node = cbind(V(top1_subG)$name, V(top1_subG)$weight)
-write.table(top1_edge,file="top1_edge.txt", row.names=FALSE, sep="\t")
-write.table(top1_node,file="top1_node.txt", row.names=FALSE, sep="\t")
-
-## All significant modules
-sig.res.mat <- res.mat[res.mat$z_perm > 1.96, ]
-module.genes <- sapply(sig.res.mat[,1], function(u)strsplit(u, split=" ")[[1]] )
-module.genes <- unique(unlist(module.genes))
-length(module.genes)
-All_subG <- induced.subgraph(G, All_genes)
-vcount(All_subG)
-All_edge = cbind(get.edgelist(All_subG), E(All_subG)$weight)
-All_node = cbind(V(All_subG)$name, V(All_subG)$weight)
-write.table(All_edge,file="All_edge.txt", row.names=FALSE, sep="\t")
-write.table(All_node,file="All_node.txt", row.names=FALSE, sep="\t")
+## Top 10 modules
+top_modules(10)
+                    
+## Top 50 modules
+top_modules(50)                   
